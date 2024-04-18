44ddc457283db76696216be728f9dffafc06e931
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-04-17 22:13:19 -0400

    Added graph, no styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+let characterGraph;
+
 async function main() {
     await CsvDataParser.parseTVData();
 
+    // next we can generate the leaflet map
+    characterBarGraph = new CharacterBarGraph({ parentElement: '#graph'}, DataStore.filteredData, "Joker");
     console.log(DataStore.filteredData)
+
+    document.getElementById("character-selection").addEventListener("change", function(){
+        value = document.getElementById("character-selection").value
+        console.log(value)
+        characterBarGraph.character = value
+        characterBarGraph.updateVis()
+    
+    })
 }
 
 main()
