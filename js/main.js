2c06537a571fd076f8770d0d92c103dda7670ebb
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-04-20 17:14:35 -0400

    Episode Selection

--- js/main.js
+++ js/main.js
@@ -4,18 +4,30 @@ async function main() {
     await CsvDataParser.parseTVData();
 
     // next we can generate the leaflet map
-    characterBarGraph = new CharacterBarGraph({ parentElement: '#graph'}, DataStore.filteredData, "Joker");
+    characterBarGraph = new CharacterBarGraph({ parentElement: '#graph'}, DataStore.filteredData, "Alfred");
     arcDiagram = new ArcDiagram({ parentElement: '#arc'}, DataStore.filteredData, CsvDataParser.sceneArray, 5)
 
     document.getElementById("character-selection").addEventListener("change", function(){
         value = document.getElementById("character-selection").value
         characterBarGraph.character = value
         characterBarGraph.updateVis()
-        arcDiagram.episode = 3
-        arcDiagram.updateVis()
-
-    
     })
+
+    document.getElementById("episodes").addEventListener("change", function() {
+        value = document.getElementById("episodes").value
+        arcDiagram.episode = value
+        arcDiagram.updateVis()
+    })
+
+    // For episodes Get the select element by its id
+    const select = document.getElementById("episodes");
+
+    for (let i = 1; i <= 60; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    select.appendChild(option);
+    }
 }
 
 main()
