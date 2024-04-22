f178a5ed81c33b2359867f551a7d9b9f265932f9
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-04-22 14:02:11 -0400

    Tooltip arcdiagram

--- js/arcDiagram.js
+++ js/arcDiagram.js
@@ -42,6 +42,7 @@ class ArcDiagram {
         vis.svg.selectAll("text").remove(); // Remove existing text elements
         vis.nodes = []
         vis.episodeCharacters = []
+        vis.links = {}
         for (const [key, value] of Object.entries(vis.sceneArray[vis.episode])) {
             value.forEach(d => {
                 if (vis.importantCharacter.includes(d) && !vis.episodeCharacters.includes(d)) {
@@ -49,6 +50,7 @@ class ArcDiagram {
                     node.name = d
                     vis.episodeCharacters.push(d)
                     vis.nodes.push(node)
+                    vis.links[d] = []
                 }
             })
         }
@@ -58,14 +60,15 @@ class ArcDiagram {
             .domain(vis.episodeCharacters);
     
         // Add nodes (circles)
-        vis.svg.selectAll("circle")
+        vis.circles = vis.svg.selectAll("circle")
             .data(vis.nodes)
             .enter()
             .append("circle")
             .attr("cx", d => vis.xScale(d.name))
             .attr("cy", vis.height - 30)
             .attr("r", 8)
-            .style("fill", "#69b3a2");
+            .style("fill", "#69b3a2")
+
     
         // Add node labels
         vis.svg.selectAll("text")
@@ -84,18 +87,42 @@ class ArcDiagram {
             for (let i = 0; i < value.length; i++) {
                 for (let j = i + 1; j < value.length; j++) {
                     if (vis.importantCharacter.includes(value[j]) && vis.importantCharacter.includes(value[i])) {
-                        vis.arcPaths.push(`M ${vis.xScale(value[i])} ${vis.height - 30} Q ${(vis.xScale(value[i]) + vis.xScale(value[j])) / 2} ${(vis.height - 300)}, ${vis.xScale(value[j])} ${vis.height - 30}`)
+                        let path = `M ${vis.xScale(value[i])} ${vis.height - 30} Q ${(vis.xScale(value[i]) + vis.xScale(value[j])) / 2} ${(vis.height - 300)}, ${vis.xScale(value[j])} ${vis.height - 30}`
+                        vis.links[value[i]].push(path)
+                        vis.links[value[j]].push(path)
+                        if (!vis.arcPaths.includes(path)) {
+                            vis.arcPaths.push(path)
+                        }
                     }
                 }
             }
-        } 
+        }
     
         vis.arcPaths.forEach(path => {
             vis.svg.append("path")
                 .attr("d", path)
                 .style("fill", "none")
-                .attr("stroke", "black");
+                .attr("stroke", "black")
         });
+
+        vis.circles
+        .on('mouseover', function(event,d){
+            vis.circles.style('fill', "#B8B8B8")
+            d3.select(this).style('fill', '#69b3b2')
+            vis.svg.selectAll("path")
+                .each(function() {
+                    let path = d3.select(this);
+                    path.style("opacity", vis.links[d.name].includes(path.attr("d")) ? 1 : 0.2)
+                });
+        })
+        .on('mouseout', function(event,d){
+            vis.circles.style('fill', "#69b3a2")
+            vis.svg.selectAll("path")
+                .each(function() {
+                    let path = d3.select(this);
+                    path.style("opacity", 2)
+                });
+        })
     }
 
     
